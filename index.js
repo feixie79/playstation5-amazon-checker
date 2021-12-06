7d508ae143e268f5933f245059759288d69562b7
Fei Xie <fei.xie-extern@moia.io>
2021-12-06 09:18:57 +0100

    check without options

--- index.js
+++ index.js
@@ -1,65 +1,37 @@
 const axios = require("axios").default;
 const cheerio = require("cheerio");
-const fs = require("fs");
 const say = require("say");
 const colors = require("colors");
 const open = require("open");
-const inquirer = require("inquirer");
-const yaml = require("js-yaml");
 
 const DETECTION_STRING_NOT_AVAILABLE = "Derzeit nicht verfügbar."; //OTHER COUNTRY, NEEDS A CHANGE
 const AVAILABLE_MESSAGE = "Auf Lager.";
+const BROWSER = "google chrome";
+const Interval = 61
+const URLs = ["https://www.amazon.de/dp/B08H93ZRK9/", "https://www.amazon.de/dp/B08H98GVK8"]
 
-const PRODUCTS_CHOICES_TO_CHECK = yaml.safeLoad(
-  fs.readFileSync("products.yaml", "utf8")
-).products;
-
-let BROWSER = "google chrome";
+let loop;
 
 async function main() {
   try {
-    let inq = await inquirer.prompt([
-      {
-        type: "checkbox",
-        choices: PRODUCTS_CHOICES_TO_CHECK,
-        message: "Choose Version(s) to check (check with spacebar)",
-        name: "version",
-      },
-      {
-        name: "interval",
-        type: "number",
-        message: "Interval to scrap (seconds)",
-        default: 30,
-      },
-      {
-        name: "browser",
-        type: "list",
-        choices: ["google chrome", "safari", "firefox"],
-        message: "Which browser",
-        default: "google chrome",
-      },
-    ]);
-    BROWSER = inq.browser;
-
     // scrap on main run and then by interval
-    inq.version.forEach(scrapProduct);
-    setInterval(() => {
-      inq.version.forEach(scrapProduct);
-    }, inq.interval * 1000);
+    URLs.forEach(scrapProduct);
+    loop = setInterval(() => {
+      URLs.forEach(scrapProduct)
+    }, Interval * 1000);
   } catch (error) {
-    console.log("error while prompting", error);
+    console.log(error);
   }
 }
 
-async function scrapProduct(uri, i) {
+async function scrapProduct(uri) {
   const AVAILABLE_SELECTOR = "#availability > span";
-
   try {
     const product_page = (
       await axios.get(uri, {
         headers: {
           "User-Agent":
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36",
         },
       })
     ).data;
@@ -68,25 +40,26 @@ async function scrapProduct(uri, i) {
     const title = $("#productTitle").text().trim();
     const price = $("#priceblock_ourprice").text().trim();
     const availableText = $(AVAILABLE_SELECTOR).text().trim();
-
-    console.log(`Checking for ${title} `);
+    const timestamp = new Date(Date.now())
+    console.log(`Checking for ${title} at ${timestamp}`);
 
     if (availableText === AVAILABLE_MESSAGE) {
-      console.log(colors.rainbow(`Price at: ${price} `));
       say.speak(title + AVAILABLE_MESSAGE);
       open(uri, { app: BROWSER });
+      clearInterval(loop);
     } else if (availableText === DETECTION_STRING_NOT_AVAILABLE) {
-      console.log(colors.red(availableText));
+      console.log(colors.yellow(availableText));
     }
   } catch (error) {
     console.error(`Error while scraping ${uri}`);
-    if (
-      error.response.data.includes(
+    if (error.response && error.response.data &&
+       error.response.data.includes(
         "To discuss automated access to Amazon data please contact"
       )
     ) {
       console.log(colors.red("Amazon is mad"));
     }
+    
   } finally {
     console.log(colors.zebra(`------------`));
   }
